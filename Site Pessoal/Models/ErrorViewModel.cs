282733cd9d2bf000516fb37d838b21998afebf4c
Nicolau Barranha <nicolaubarranha999@hotmail.com>
2021-01-09 15:34:42 +0000

    Add project files.

---
+++ Site Pessoal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Site_Pessoal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
