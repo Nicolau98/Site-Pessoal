4b3d31b7a951ec156564fbba7fd63405dba92d24
Nicolau Barranha <nicolaubarranha999@hotmail.com>
2021-02-03 04:45:15 +0000

    login start

---
+++ Site Pessoal/Controllers/Conta.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site_Pessoal.Controllers
+{
+    public class Conta : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+    }
+}
---
+++ Site Pessoal/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site_Pessoal.Models
+{
+    public class LoginViewModel
+    {
+        public string Utilisador { get; set; }
+
+        public string Password { get; set; }
+
+        public bool Lembrar { get; set; }
+    }
+}
